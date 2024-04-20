4fa5e4e22c1213dfc69bc78c73e4d9559299b974
Siron Shakya <sironshakya10@gmail.com>
2024-04-20 21:01:48 +0545

    s3 removed

--- testproject/api/views.py
+++ testproject/api/views.py
@@ -3,7 +3,6 @@ from django.http import FileResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import File
-import boto3
 
 
 @api_view(['GET', 'POST'])
@@ -15,20 +14,7 @@ def index(request, id):
             file = File.objects.get(id=id)
 
             if file is not None:
-                print("file found", file.filename)
-                s3 = boto3.client('s3')
-                bucket_name = 'siron-bucket'
-                key = file.filename
-
-                response = s3.get_object(Bucket=bucket_name, Key=key)
-                content_type = response['ContentType']
-                data_bytes = response['Body']
-                print(content_type)
-                response = FileResponse(
-                    data_bytes,
-                    content_type=content_type
-                )
-                return response
+                return Response({"filename": file.filename}, status=200)
             else:
                 return Response("File not found in DB", status=404)
         except Exception as e:
